8b25e77d86286a93e267254ad278683f7ff4ec07
ayadalshaikhli <79942630+ayadalshaikhli@users.noreply.github.com>
2021-07-02 22:07:14 -0700

    created a new folder and added the video link to the readme

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# e-com
+## e-commerce
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+
+  <h3 align="center">E-Commerce</h3>
+
+  <p align="center">
+    This application is designed for companies to track their products by category, product, and tag.
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#usage">Credits</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+e-commerce back end which allows for creation, review, update, and deletion
+
+### Built With
+
+- [JavaScript](https://www.javascript.com/)
+- [MySQL](https://www.mysql.com/)
+- [Express](https://expressjs.com/)
+- [Dotenv](https://www.npmjs.com/package/dotenv)
+- [Sequelize](https://sequelize.org/v3/)
+
+## Usage
+
+WHEN I add my database name, MySQL username, and MySQL password to an environment variable file, THEN I am able to connect to a database using Sequelize
+WHEN I enter schema and seed commands, THEN a development database is created and is seeded with test data
+WHEN I enter the command to invoke the application, THEN my server is started and the Sequelize models are synced to the MySQL database
+WHEN I open API GET routes in Insomnia Core for categories, products, or tags, THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia Core, THEN I am able to successfully create, update, and delete data in my database
+
+- [Usage Video](https://youtu.be/PgoxhxdpR1Y)
+
+## Credits
+
+1. https://www.w3schools.com/
+2. https://stackoverflow.com/
+3. https://github.com/
+4. https://expressjs.com/
+5. https://www.npmjs.com/package/dotenv
+6. https://www.mysql.com/
---
+++ routes/api/category-routes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async(req, res) => {
+  // find all categories
+  try{
+    const categoryDataAll = await Category.findAll({
+      include: [{model:Product}]
+    });
+    res.status(200).json(categoryDataAll);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // be sure to include its associated Products
+});
+
+router.get('/:id', async(req, res) => {
+  // find one category by its `id` value
+  try{
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryData)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  // be sure to include its associated Products
+});
+
+router.post('/', async(req, res) => {
+  // create a new category
+  try{
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(400).json(err)
+  }
+});
+
+router.put('/:id', async(req, res) => {
+  // update a category by its `id` value
+  try{
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id,
+        }
+      },
+    );
+
+    return res.json(categoryData)
+  } catch (err) {
+    res.json(err)
+  }
+});
+
+router.delete('/:id', async(req, res) => {
+  // delete a category by its `id` value
+  try{
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!categoryData) {
+      res.status(404).json({message: 'No cateogry found with that id!'});
+      return;
+    }
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+module.exports = router;
